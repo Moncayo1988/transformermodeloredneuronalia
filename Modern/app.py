921faf2b87d483a11913597b148fd875f966eeef
Andrés Felipe Garcés Campo <felipegarces2468@gmail.com>
2026-05-28 23:09:47 -0500

    Correción de rutas para el render.yaml y requierement_api.txt además de añadir el archivo de subir_modelo.py

--- Modern/app.py
+++ Modern/app.py
@@ -3,6 +3,17 @@ from fastapi.middleware.cors import CORSMiddleware
 import uvicorn, io, cv2, numpy as np
 from PIL import Image
 from Modern.main import procesar_imagen   # importa tu pipeline
+from huggingface_hub import hf_hub_download
+import os
+
+# --- DESCARGA DEL MODELO DESDE HUGGING FACE ---
+# Se ejecuta una sola vez al arrancar el servidor en Render
+MODEL_PATH = hf_hub_download(
+    repo_id="Huntercito/Deteccion_Pico_y_Placa",  # Tu repositorio real
+    filename="transformer_pico_placa.pt",
+    token=os.environ.get("HF_TOKEN")  # Se leerá de las variables de entorno en Render
+)
+# -----------------------------------------------
 
 app = FastAPI(title="Detección de Placas - Popayán")
 
